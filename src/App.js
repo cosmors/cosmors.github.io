9e00227cb7e43fb670f6a0ab1a189517a6f5c97a
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-11-23 04:55:47 -0300

    Page Not Found resolved

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <BrowserRouter>
         <Switch>
-          <Route exact path="/cosmors.github.io/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
