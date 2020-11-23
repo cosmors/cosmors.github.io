e7f3048eb87c04a82432cc9d0e099e976a0f7c8b
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-11-23 04:16:28 -0300

    Starting a new project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import NotFound from './components/NotFound'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/cosmors" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+export default () => <header> <h1>My Personal site</h1> </header>;
