1373dffd88f7da28cd8df6e49004850c2e80a4ea
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-15 03:10:04 -0300

    Project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Header from '../components/Header';
+import movieLbrary from '../img/movie-library.png';
+import onlineStorage from '../img/online-storage.png';
+import ticTacToe from '../img/tic-tac-toe.png';
+import toDo from '../img/to-do.png';
 function Projects() {
   return (
     <div className="project">
       <Header />
       <div className="container">
-        <h1>Projects Page</h1>
+        <section>
+          <img src={onlineStorage} />
+        </section>
+        <section>
+          <img src={ticTacToe} />
+        </section>
+        <section>
+          <img src={toDo} />
+        </section>
+        <section>
+          <img src={movieLbrary} />
+        </section>
       </div>
     </div>
   )
