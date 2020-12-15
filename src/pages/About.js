293a1bfeca0718e51f24641fb3cfcdd828aba745
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-14 23:55:22 -0300

    Some text about

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,9 +15,11 @@ export class About extends Component {
         <div className="container">
           <section>
             <img src={apaixonado} />
+            <p>Every great man has a great woman by your side. This is mine. Friend, companion, advisor and my Play 2. She always has a friendly word, increible advises, and corrections when I need. We are together until death do us part.</p>
           </section>
           <section>
             <img src={paizao} />
+            <p>What can I could be without my followers? This little guys make me wake up everyday (literally) with a smiling in my face. They are my every reason, all my 'because's'. If you lost your faith in heroes, you don't know what is to be father yet!</p>
           </section>
           <section>
             <img src={mochileiro} />
