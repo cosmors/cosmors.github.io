3713949d12416d2e44f6d761f1f60f1162428929
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-14 22:00:05 -0300

    Some images about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 import Header from '../components/Header';
+import apaixonado from '../img/apaixonado.jpg';
+import paizao from '../img/paizao.JPG';
+import mochileiro from '../img/trilheiro.jpg';
+import guerreiro from '../img/guerreiro.jpg';
+import musico from '../img/musico.jpg';
+
 
 export class About extends Component {
   render() {
@@ -7,7 +13,21 @@ export class About extends Component {
       <div className="about">
         <Header />
         <div className="container">
-          <h1>About Page</h1>
+          <section>
+            <img src={apaixonado} />
+          </section>
+          <section>
+            <img src={paizao} />
+          </section>
+          <section>
+            <img src={mochileiro} />
+          </section>
+          <section>
+            <img src={guerreiro} />
+          </section>
+          <section>
+            <img src={musico} />
+          </section>
         </div>
       </div>
     )
